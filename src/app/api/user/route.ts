dfd576806fe1bd51cafced1681a9df6aae21f32f
Bon Lacorte <bonbonlacorte123@gmail.com>
2024-10-16 21:21:01 +0800

    fix auth folder

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,10 @@
+// // Get the user data
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/lib/auth";
+// import { NextResponse } from "next/server";
+
+// export const GET = async () => {
+//     const session = await getServerSession(authOptions)
+
+//     return NextResponse.json({ authenticated: !!session })
+// }
